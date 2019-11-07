c467a8f1e789e69b0909a1f91db8d7e6229ed5c2
Angel <angelus_auditore@hotmail.com>
2019-11-07 13:11:47 -0600

    clase 2

---
+++ clase2/ejercicioDiccionarios.py
@@ -0,0 +1,9 @@
+#paso 1
+personajes={1:"David",2:"Walter",3:"Vader"}
+#paso 2
+personajes["Bob"]="Darth Vader"
+personajes["Len"]="Ezio"
+#paso 3
+personajes["Bob"]="Harry Potter"
+personajes["Len"]="Dobby"
+#paso 4
---
+++ clase2/ejercicioListas.py
@@ -0,0 +1,19 @@
+#paso 1
+series=["s1","s2","s3","s4"]
+#paso 2
+series.append("s5")
+series.append("s6")
+series.append("s7")
+print(series)
+#paso 3
+series[5]="sn"
+print(series)
+#paso 4
+series.remove("s1")
+print(series)
+#paso 5
+sus_series=["n1","n2","n3"]
+print(sus_series)
+#paso 6
+nSerie=series+sus_series
+print(nSerie)
