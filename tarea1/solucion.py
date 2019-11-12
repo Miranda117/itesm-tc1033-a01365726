67c039190a52348915fb8e232683ebd44d6d24d0
Angel <angelus_auditore@hotmail.com>
2019-11-11 22:28:39 -0600

    se añade el csv al mismo directorio que solucion.py

---
+++ tarea1/solucion.py
@@ -0,0 +1,10 @@
+def diccionaryStructure():
+    dic_data={}
+    with open('datos_vuelos.csv','r') as data:
+        datalines=data.readlines()
+        print(datalines)
+        print("HOla")
+        #for l in datalines:
+         #   data=l.split("")
+          #  iD=data[0]
+
