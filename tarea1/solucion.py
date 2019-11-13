a9c660c0bb82511d580156ac00b6c0606c7743c3
Angel <angelus_auditore@hotmail.com>
2019-11-12 20:47:05 -0600

    planteamiento en la organizacion del archivo

--- tarea1/solucion.py
+++ tarea1/solucion.py
@@ -1,10 +1,36 @@
 def diccionaryStructure():
     dic_data={}
-    with open('datos_vuelos.csv','r') as data:
-        datalines=data.readlines()
-        print(datalines)
-        print("HOla")
-        #for l in datalines:
-         #   data=l.split("")
-          #  iD=data[0]
+    dic_dates={}
+    data= open('datos_vuelos.csv','r')
+    datalines=data.readlines()
+    #datalines=datalines.replace(","," ")
+    #pos=datalines.find("\n")
+    #datalinesN=datalines[pos:]
+    #datalinesR=datalinesN.readlines()
+    #datalinesF=datalinesN.split(",")
 
+    for x in datalines:
+        data=x.split(",")
+        #p=0+(x*4)
+        iD=data[0]
+        date=data[1]
+        if iD not in dic_data.keys():
+            dic_data[iD]={}
+            
+            if date not in dic_data[iD].keys():
+                dic_dates[date]={}
+                if date not in dic_data[date].keys():
+                    dic_data[iD][date]=1
+            else:
+                dic_data[iD][date]+=1
+   # x=9
+    #p=0+(x*4)-x
+    #data.close()
+    print(dic_data)
+    return dic_data
+    
+    
+
+
+
+diccionaryStructure()
