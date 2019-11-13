2624d5f7c85a0143159c00d4e914052d08485503
Angel <angelus_auditore@hotmail.com>
2019-11-12 23:34:08 -0600

    Identificacion de numero de vuelos por matricula del automovil
    en este punto, se realizan los filtros necesarios en un sistema de listas para despues ser mostrado como un diccionario

--- tarea1/solucion.py
+++ tarea1/solucion.py
@@ -1,33 +1,67 @@
-def diccionaryStructure():
-    dic_data={}
-    dic_dates={}
-    data= open('datos_vuelos.csv','r')
-    datalines=data.readlines()
-    #datalines=datalines.replace(","," ")
-    #pos=datalines.find("\n")
-    #datalinesN=datalines[pos:]
-    #datalinesR=datalinesN.readlines()
-    #datalinesF=datalinesN.split(",")
+def diccionaryConverter(iDtmp_list):
+    return{i:iDtmp_list.count(i) for i in iDtmp_list}
 
-    for x in datalines:
-        data=x.split(",")
+def diccionaryStructure():
+    iD=[]
+    dates=[]
+    #dic_data={}
+    #dic_dates={}
+    data= open('datos_vuelos.csv','r')
+    datalines=data.read()
+    #datalines=datalines.replace(","," ")
+    pos=datalines.find("\n")
+    datalinesN=datalines[pos+1:]
+    #datalinesR=datalinesN.readlines()
+    datalinesF=datalinesN.split(",")
+
+    for x in range (len(datalinesF)):
+        p=0+(x*4)-x
+        #pd=1+(x*4)-x
+        if (len(datalinesF))>=p:
+            iD.append(datalinesF[p])
+            #dates.append(datalinesF[pd])
+        elif (len(datalinesF))<p:
+            pass#print("-")
+    #print(datalinesF[1])    
+    iDchain="\n".join(iD)
+    iDchain=iDchain.replace("\n",",")
+    iDchain=iDchain.replace(" ","")
+    iDtmp_list=iDchain.split(",")
+    #print(iDtmp_list)
+    pos=0
+    while pos<=(len(iDtmp_list))/2+14:
+        posD=pos+1
+        del(iDtmp_list[posD])
+        pos=pos+1
+    del(iDtmp_list[31])
+
+    result=diccionaryConverter(iDtmp_list)
+    print(result)
+    
+
+    #print(dates)
+        #print(datalinesF[p])
+        #print(datalinesF[pd])
+    
+   #for x in datalines:
+    #    data=x.split(",")
         #p=0+(x*4)
-        iD=data[0]
-        date=data[1]
-        if iD not in dic_data.keys():
-            dic_data[iD]={}
+     #   iD=data[0]
+      #  date=data[1]
+       # if iD not in dic_data.keys():
+        #    dic_data[iD]={}
             
-            if date not in dic_data[iD].keys():
-                dic_dates[date]={}
-                if date not in dic_data[date].keys():
-                    dic_data[iD][date]=1
-            else:
-                dic_data[iD][date]+=1
-   # x=9
-    #p=0+(x*4)-x
+         #   if date not in dic_data[iD].keys():
+          #      dic_dates[date]={}
+           #     if date not in dic_data[date].keys():
+            #        dic_data[iD][date]=1
+            #else:
+             #   dic_data[iD][date]+=1
+                
+    
     #data.close()
-    print(dic_data)
-    return dic_data
+    #print(dic_data)
+    #return dic_data
     
     
 
